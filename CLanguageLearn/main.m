c9cf1f2c0b657d2ef2825996d24ffa91ce4e6310
xiexiaolong <mgfjxxiexiaolong@gmail.com>
2016-07-07 16:58:27 +0800

    Initial Commit

---
+++ CLanguageLearn/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CLanguageLearn
+//
+//  Created by 谢小龙 on 16/7/7.
+//  Copyright © 2016年 XXL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
