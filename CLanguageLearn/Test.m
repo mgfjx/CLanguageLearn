e0a55bc2520008df695c74f1ee17b0566f01900c
xiexiaolong <mgfjxxiexiaolong@gmail.com>
2016-07-11 15:46:04 +0800

    文件读取操作

---
+++ CLanguageLearn/Test.m
@@ -0,0 +1,15 @@
+//
+//  Test.m
+//  CLanguageLearn
+//
+//  Created by 谢小龙 on 16/7/11.
+//  Copyright © 2016年 XXL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+void shabi(){
+    
+    printf("shabi\n");
+    
+}
--- CLanguageLearn/main.m
+++ CLanguageLearn/main.m
@@ -8,85 +8,30 @@
 
 #import <Foundation/Foundation.h>
 
-struct Student{
-    
-    char cName[20];
-    int iNumber;
-    struct Student *pNext;
-    
-};
-
-int iCount;
-
-struct Student* Create(){
-    
-    struct Student *pHead = NULL;
-    struct Student *pEnd, *pNew;
-    iCount = 0;
-    
-    pEnd = pNew = malloc(sizeof(struct Student));
-    
-    printf("first enter Name, then Number\n");
-    scanf("%s", &pNew->cName);
-    scanf("%d", &pNew->iNumber);
-    
-    while (pNew->iNumber != 0) {
-        
-        iCount ++;
-        if (iCount == 1) {
-            pNew->pNext = pHead;
-            pEnd = pNew;
-            pHead = pNew;
-        }else{
-            
-            pNew->pNext = NULL;
-            pEnd->pNext = pNew;
-            pEnd = pNew;
-            
-        }
-        
-        pNew = malloc(sizeof(struct Student));
-        
-        scanf("%s", &pNew->cName);
-        scanf("%d", &pNew->iNumber);
-    }
-    
-    free(pNew);
-    
-    return pHead;
-}
-
-
-void Print(struct Student *pHead){
-    
-    struct Student *pTemp;
-    int index = 1;
-    
-    printf("----------这个链表有%d个成员----------\n",iCount);
-    
-    pTemp = pHead;
-    
-    while (pTemp != NULL) {
-        
-        printf("成员%d是: \n",index);
-        printf("姓名是: %s\n",pTemp->cName);
-        printf("学号是: %d\n",pTemp->iNumber);
-        printf("-------------------------\n");
-        pTemp = pTemp->pNext;
-        index++;
-        
-    }
-    
-}
-
-
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        struct Student *p = Create();
+        FILE *fp, *fp2;
         
-        Print(p);
+        fp = fopen("/Users/xiexiaolong1/Desktop/sdxl.txt", "r+");
+        fp2 = fopen("/Users/xiexiaolong1/Desktop/cfile.txt", "r+");
         
+        if (fp == NULL && fp2 == NULL) {
+            printf("打开文件失败");
+            return 0;
+        }
+        
+        char ch[100];
+        
+        fgets(ch, 100, fp);
+        
+        while (fgets(ch, 100, fp) != NULL) {
+//            puts(ch);
+            fputs(ch, fp2);
+        }
+        
+        fclose(fp);
+        fclose(fp2);
     }
     return 0;
 }
